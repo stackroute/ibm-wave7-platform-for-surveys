c18eba92bac2bd29e9e880715e91c0523bcbb437
Spoorthi Samala <spoorthisamala@gmail.com>
2019-08-26 11:27:00 +0530

    CRUDoperations added

---
+++ survey-service/src/main/java/com/stackroute/surveyservice/service/SurveyService.java
@@ -0,0 +1,16 @@
+package com.stackroute.surveyservice.service;
+
+import com.stackroute.surveyservice.domain.Survey;
+
+import java.util.List;
+
+public interface SurveyService {
+    public Survey saveSurvey(Survey survey);
+
+    public boolean deleteSurvey(int id);
+
+    public List<Survey> getAllSurveys();
+
+    public Survey updateSurvey(Survey survey);
+
+}
