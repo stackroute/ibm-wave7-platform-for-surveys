d038ad4ecc9e3e30f014181a740e6bf56f7511a0
Spoorthi Samala <spoorthisamala@gmail.com>
2019-09-03 09:56:23 +0530

    user-response service added

---
+++ user-response/src/main/java/com/stackroute/userresponse/domain/Response.java
@@ -0,0 +1,23 @@
+package com.stackroute.userresponse.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class Response {
+
+    @Id
+    private String question_id;
+
+    private String response;
+}
