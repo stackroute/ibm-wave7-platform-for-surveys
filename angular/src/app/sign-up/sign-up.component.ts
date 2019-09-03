ea46207c50600e810cc7f6c1531d53b92f51a5a1
Sahithi <sahithimpc@gmail.com>
2019-09-03 11:24:39 +0530

    Added validations for every field in the signup page

--- angular/src/app/sign-up/sign-up.component.ts
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -3,6 +3,7 @@ import { UserRegistrationService } from '../user-registration.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../modals/User';
 import { Router } from '@angular/router';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -17,6 +18,22 @@ export class SignUpComponent implements OnInit {
     private route: ActivatedRoute,  private router : Router) { }
 
   ngOnInit() {
+  }
+  nameFormControl = new FormControl('', [Validators.required]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  biodataForm: FormGroup = new FormGroup({
+    email: this.emailFormControl,
+    name: this.nameFormControl,
+  });
+  getEmailErrorMessage() {
+    return this.emailFormControl.hasError('required') ? 'You must enter a value' :
+      this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
+  }
+  getRequiredErrorMessage(field) {
+    return this.biodataForm.get(field).hasError('required') ? 'You must enter a value' : '';
   }
   saveUser(user:User) {
       this.registrationService.saveUser(user).subscribe((data)=> {
