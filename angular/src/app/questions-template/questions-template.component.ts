6f7720cc5e903b3495bd411bf8a3c36cc731a3ef
Sahithi <sahithimpc@gmail.com>
2019-09-05 18:05:34 +0530

    Added drag and drop functionality for the questions-template

--- angular/src/app/questions-template/questions-template.component.ts
+++ angular/src/app/questions-template/questions-template.component.ts
@@ -1,128 +1,153 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { SurveyService } from '../survey.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Question } from '../modals/Question';
-import { Survey } from '../modals/Survey';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { HttpClient } from '@angular/common/http';
-import { Location } from '@angular/common';
-
+import { Component, OnInit, Inject } from "@angular/core";
+import { SurveyService } from "../survey.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Question } from "../modals/Question";
+import { Survey } from "../modals/Survey";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { HttpClient } from "@angular/common/http";
+import { Location } from "@angular/common";
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-questions-template',
-  templateUrl: './questions-template.component.html',
-  styleUrls: ['./questions-template.component.scss']
+  selector: "app-questions-template",
+  templateUrl: "./questions-template.component.html",
+  styleUrls: ["./questions-template.component.scss"]
 })
 export class QuestionsTemplateComponent implements OnInit {
-
+  
   private condition: boolean;
-
   private choiceVisibility: boolean;
   private count: number;
   private question: Question;
   private survey: Survey;
-  private questionList:Question[];
-  private newchoices : string[] = [];
+  private questionList: Question[];
+  private recommendedQuestionList: Question[];
+  private newchoices: string[] = [];
   private url;
 
-  constructor(private surveyService: SurveyService, private route: ActivatedRoute,
-    private dialog : MatDialog,private location:Location,private httpClient:HttpClient,private router:Router) { }
-   
-   
-  ngOnInit() {
+  constructor(
+    private surveyService: SurveyService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private location: Location,
+    private httpClient: HttpClient,
+    private router: Router
+  ) {}
 
-    this.getQuestionList(this.surveyService.surveyId);
-    this.url=window.location.href;
+  ngOnInit() {
+    this.getQuestionList(this.surveyService.editSurvey.id);
+    this.getRecommendedQuestions(this.surveyService.editSurvey.domain_type);
+    this.url = window.location.href;
   }
-  publish()
-  {
-    this.surveyService.sendMail(this.url).subscribe(
-      (data) =>{
-        console.log(data);
-      });
-      this.surveyService.publishedURL = this.url;
-        this.router.navigateByUrl('publishview')
+ 
+
+  drop(event: CdkDragDrop<Object[]>) {
+    
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+      
+    }
+  }
+
+  publish() {
+    this.surveyService.sendMail(this.url).subscribe(data => {
+      console.log(data);
+    });
+    this.surveyService.publishedURL = this.url;
+    this.router.navigateByUrl("publishview");
   }
 
   addQuestion() {
     this.condition = true;
   }
 
-  deleteQuestion(question : Question)
-  {
-   this.surveyService.deleteQuestion(question).subscribe(
-     (data) => {
-         console.log(data);
-         this.getQuestionList(this.surveyService.surveyId);
-     })
+  deleteQuestion(question: Question) {
+    this.surveyService.deleteQuestion(question).subscribe(data => {
+      console.log(data);
+      this.getQuestionList(this.surveyService.editSurvey.id);
+    });
   }
-
-  addChoice(choiceText : string) {
-
-    this.newchoices.push(choiceText);
-  }
-
-  saveQuestion(question: Question) {
-    question.choices = this.newchoices;
-    console.log("questin from ts",question);
-    this.surveyService.saveQuestion(question).subscribe((data) => {
-      this.question = data;
-      console.log("result is ", this.question);
-      this.getQuestionList(this.surveyService.surveyId);
+  getRecommendedQuestions(domain: String) {
+    this.surveyService.getRecommendedQuestions(domain).subscribe(data => {
+      this.recommendedQuestionList = data;
+      console.log("Recommended Questions : ", this.recommendedQuestionList);
     });
   }
 
-  editQuestion(question){
+  addChoice(choiceText: string) {
+    this.newchoices.push(choiceText);
+    (<HTMLInputElement>document.getElementById("choiceText")).value = '';
+  }
+
+  saveQuestion(form: NgForm) {
+    let question = form.value;
+    question.choices = this.newchoices;
+    console.log("questin from ts", question);
+    this.surveyService.saveQuestion(question).subscribe((data) => {
+      this.question = data;
+      console.log("result is ", this.question);
+      this.getQuestionList(this.surveyService.editSurvey.id);
+      this.condition = false;
+      form.reset();
+      this.newchoices = [];
+    });
+
+  }
+
+  editQuestion(question) {
     console.log(question);
 
-    const dialogRef = this.dialog.open(EditQuestionDialog,
-      {
-        width: '250px',
-        data: {question}
-      });
+    const dialogRef = this.dialog.open(EditQuestionDialog, {
+      width: "250px",
+      data: { question }
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result != undefined) {
         console.log(result);
-        this.surveyService.editQuestion(result.question).subscribe(
-          (data) => {
-            console.log(data);
-            console.log("Choices",data.choices);
-            this.getQuestionList(this.surveyService.surveyId);
-          })
+        this.surveyService.editQuestion(result.question).subscribe(data => {
+          console.log(data);
+          console.log("Choices", data.choices);
+          this.getQuestionList(this.surveyService.surveyId);
+        });
       }
     });
   }
 
   getQuestionList(surveyId: string) {
-    this.surveyService.getAllQuestions(surveyId).subscribe(
-      (data) => {
+    this.surveyService.getAllQuestions(surveyId).subscribe(data => {
       this.questionList = data.questionList;
-        console.log("questions : ", this.questionList)
-      })
+      console.log("questions : ", this.questionList);
+    });
   }
 }
 
-
 @Component({
-  selector: 'editQuestionDialog',
-  templateUrl: 'editQuestionDialog.html',
+  selector: "editQuestionDialog",
+  templateUrl: "editQuestionDialog.html"
 })
-
 export class EditQuestionDialog {
-
   constructor(
-
     public dialogRef: MatDialogRef<EditQuestionDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: Question) { }
+    @Inject(MAT_DIALOG_DATA) public data: Question
+  ) {}
 
-    private editQuestion : Question
+  private editQuestion: Question;
 
-    ngOnInit()
-    {
-        this.editQuestion = this.data;
-        console.log(this.data);
-    }
+  ngOnInit() {
+    this.editQuestion = this.data;
+    console.log(this.data);
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
