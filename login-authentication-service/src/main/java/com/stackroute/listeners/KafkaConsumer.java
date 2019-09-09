a5fd6b6a0f8f50d82c304199d11aba01bcbdecc2
Rupasree <rupasree045@gmail.com>
2019-09-09 18:06:09 +0530

    added getSurveysByDomainName in survey

--- login-authentication-service/src/main/java/com/stackroute/listeners/KafkaConsumer.java
+++ login-authentication-service/src/main/java/com/stackroute/listeners/KafkaConsumer.java
@@ -25,7 +25,7 @@ public class KafkaConsumer {
     @KafkaListener(topics = "UserRegistration", groupId = "group_id")
     public void consume(String daoUser) throws IOException {
        DAOUser obj = new ObjectMapper().readValue(daoUser, DAOUser.class);
-        System.out.printf(daoUser);
+        System.out.printf("----------------------------"+daoUser);
         obj.setPassword(bcryptEncoder.encode(obj.getPassword()));
         userDao.save(obj);
     }
--- survey-service/src/main/java/com/stackroute/surveyservice/configuration/KafkaConfiguration.java
+++ survey-service/src/main/java/com/stackroute/surveyservice/configuration/KafkaConfiguration.java
@@ -23,7 +23,7 @@ public class KafkaConfiguration {
 
             Map<String, Object> config = new HashMap<>();
 
-            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.238.196:9092");
+            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
             config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
             config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
