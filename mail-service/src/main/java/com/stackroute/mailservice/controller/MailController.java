74a55ff014b454c0df250224cf587e37ec62f0c7
AghaZafeer <agha.zafeer@gmail.com>
2019-09-10 10:40:19 +0530

    Resolved merge conflicts

--- mail-service/src/main/java/com/stackroute/mailservice/controller/MailController.java
+++ mail-service/src/main/java/com/stackroute/mailservice/controller/MailController.java
@@ -3,6 +3,7 @@ package com.stackroute.mailservice.controller;
 import com.stackroute.mailservice.model.Mail;
 import com.stackroute.mailservice.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.MessagingException;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +22,9 @@ public class MailController {
     @Autowired
     private Mail mail;
 
-<<<<<<< HEAD
+
     @PostMapping("send-mail")
-    public String send(@RequestBody Mail mail) {
+    public ResponseEntity send(@RequestBody Mail mail) {
         Map<String, Object> map = new HashMap<String, Object>();
 //        String url=mail.getUrl();
         System.out.println(mail.getUrl());
@@ -33,23 +34,13 @@ public class MailController {
 
         try {
             mailService.sendMail(map);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-=======
-    @RequestMapping("send-mail")
-    public ResponseEntity<Mail> send(@RequestParam String url) {
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("url", url);
-        model.put("signature", "Mail to take the survey");
-        try {
-            mailService.sendMail(model);
         }
+
         catch (MessagingException | IOException | javax.mail.MessagingException e) {
->>>>>>> 17a3f40861f265b2baf36d99dbe67af216e0aad0
+
             e.printStackTrace();
         }
 
-        return new ResponseEntity<>();
+        return new ResponseEntity("Mail Sent", HttpStatus.OK);
     }
 }
