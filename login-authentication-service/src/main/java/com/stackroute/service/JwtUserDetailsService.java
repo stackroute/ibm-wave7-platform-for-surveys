40e8cefda2351a3e40c94d597430895b2a14de38
Sahithi <sahithimpc@gmail.com>
2019-09-13 11:02:19 +0530

    Modified docker-compose.yml

--- login-authentication-service/src/main/java/com/stackroute/service/JwtUserDetailsService.java
+++ login-authentication-service/src/main/java/com/stackroute/service/JwtUserDetailsService.java
@@ -46,35 +46,35 @@ public class JwtUserDetailsService implements UserDetailsService {
 		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
 		return userDao.save(newUser);
 	}
-	public String forgotPassword(String username) throws  javax.mail.MessagingException {
-		String status = "Failed";
-		System.out.println(username);
-		System.out.println(userDao.findByUsername(username));
-		System.out.println("abcd");
-		if (userDao.findByUsername(username) != null) {
-			System.out.println(username);
-			System.out.println("efgh");
-			MimeMessage message=javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-			helper.setTo("rdevi2850@gmail.com");
-			helper.setSubject("Link for Reset your Password");
-			helper.setText("http://localhost:4200/reset-password");
-			javaMailSender.send(message);
-			System.out.println("hello");
-			status = "Sent";
-		}
-		else {
-
-		}
-		return status;
-	}
-
-	//    @Override
-	public DAOUser update(UserDTO userDTO) throws Exception {
-		DAOUser user = userDao.findByUsername(userDTO.getUsername());
-		if (user != null) {
-			user.setPassword(bcryptEncoder.encode(userDTO.getPassword()));
-		}
-		return userDao.save(user);
-	}
+//	public String forgotPassword(String username) throws  javax.mail.MessagingException {
+//		String status = "Failed";
+//		System.out.println(username);
+//		System.out.println(userDao.findByUsername(username));
+//		System.out.println("abcd");
+//		if (userDao.findByUsername(username) != null) {
+//			System.out.println(username);
+//			System.out.println("efgh");
+//			MimeMessage message=javaMailSender.createMimeMessage();
+//			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//			helper.setTo("rdevi2850@gmail.com");
+//			helper.setSubject("Link for Reset your Password");
+//			helper.setText("http://localhost:4200/reset-password");
+//			javaMailSender.send(message);
+//			System.out.println("hello");
+//			status = "Sent";
+//		}
+//		else {
+//
+//		}
+//		return status;
+//	}
+//
+//	//    @Override
+//	public DAOUser update(UserDTO userDTO) throws Exception {
+//		DAOUser user = userDao.findByUsername(userDTO.getUsername());
+//		if (user != null) {
+//			user.setPassword(bcryptEncoder.encode(userDTO.getPassword()));
+//		}
+//		return userDao.save(user);
+//	}
 }
